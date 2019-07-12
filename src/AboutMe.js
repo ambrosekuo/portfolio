a8a37aba4955222ec312f72c8854d93cd2e8646c
ambrose214 <ambrose.kuo@hotmail.com>
2019-07-12 05:03:57 -0400

    just adding profile text

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -8,15 +8,12 @@ function AboutMe() {
         <img src="/assets/me.jpg" alt="Me" />
         <div className="About-Me-Description">
           <h1> ABOUT ME</h1>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          <p>
+            Hello! I'm Ambrose Kuo, a 3rd year Computer Science student at
+            Ryerson University. These are some projects I'm working on!{" "}
+          </p>
+          <a href="https://github.com/ambrosekuo"> Github</a>
+          <a href="https://github.com/ambrosekuo"> Linkedln</a>
         </div>
       </div>
     </div>
